b024bb5842e3e621ec392333ce76ac8a60912a31
Muhammad Ibrahim Isah <muhammadibrahimisah0@gmail.com>
2024-04-16 11:43:22 +0100

    adding new feature to master branch

--- src/TeacherRecord.java
+++ src/TeacherRecord.java
@@ -1,6 +1,7 @@
 public class TeacherRecord {
     public static void main(String[] args) {
         System.out.println("Teacher Record");
+
         System.out.println("For master branch");
     }
 }
